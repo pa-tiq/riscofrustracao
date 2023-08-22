09087496d05825857e62911dc32b791e96a3bc1a
AUX-SSI <patrickdsilva99@gmail.com>
2023-08-22 12:22:42 -0400

    fixed cookies and styles

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ import Footer from './common/template/footer';
 //import Messages from './common/msg/messages';
 
 import { useCookies } from 'react-cookie';
+import { useEffect } from 'react';
 
 const App = () => {
   const [cookie] = useCookies(['menuWideCookie']);
   const { menuWideCookie } = cookie;
 
+  useEffect(()=>{
+    document.getElementById('app_body').classList.toggle('sidebar-collapse');
+    console.log(document.getElementById('app_body'));
+  },[menuWideCookie]);
+
   return (
-    <div
-      className={
-        menuWideCookie === 'false'
-          ? 'fixed sidebar-mini sidebar-collapse'
-          : 'fixed sidebar-mini'
-      }
-    >
       <div className='wrapper' data-cliente={'risco'}>
         <Header />
         <div className='content-wrapper'>
@@ -27,7 +26,6 @@ const App = () => {
         <Footer />
         {/* <Messages /> */}
       </div>
-    </div>
   );
 };
 
