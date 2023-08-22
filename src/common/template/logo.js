f86c48aaec601a8650f2976cbff97b3b84040664
AUX-SSI <patrickdsilva99@gmail.com>
2023-08-22 14:49:03 -0400

    new logo

--- src/common/template/logo.js
+++ src/common/template/logo.js
@@ -6,7 +6,7 @@ const Logo = ({ logoName }) => {
           regius: <img src={require('../../assets/imgs/logo_regius.png')} />, //regius
           wedan: <img src={require('../../assets/imgs/wedan.jpg')} />, //wti
           prevsan: <img src={require('../../assets/imgs/logo_prevsan.png')} />, //previsan
-          risco: <img src={require('../../assets/imgs/logo_risco.png')} />, //risco
+          risco: <img src={require('../../assets/imgs/eletronico.png')} alt="AI Icon by Darius Dan" />, //risco
         }[logoName]
       }
     </div>
